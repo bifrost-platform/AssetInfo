5035f9b7fe14b86ff25d4f9d75aa5da42f04a359
jormal <jormal@naver.com>
2024-01-23 14:13:06 +0900

    Fix repo settings

--- script/downscale.py
+++ script/downscale.py
@@ -1,9 +1,7 @@
-
 from argparse import ArgumentParser
 from glob import glob
 from logging import INFO, basicConfig, getLogger
 from os import makedirs, path
-from re import I
 
 from PIL import Image
 
@@ -15,10 +13,12 @@ BASE_PATH = "/Users/jormal/Documents/Repositories/AssetInfo/Assets"
 CHAINS = [
     "arbitrumgoerli",
     "arbitrumone",
+    "arbitrumsepolia",
     "avalanche",
     "baobab",
     "base",
     "basegoerli",
+    "basesepolia",
     "bifrost",
     "bifrosttest1",
     "binance",
@@ -31,19 +31,15 @@ CHAINS = [
     "optimism",
     "optimismgoerli",
     "polygon",
+    "sepolia",
 ]
 TOKEN_PATH = "tokens/images"
-SIZES = {
-    "256": 256,
-    "128": 128,
-    "32/x4": 128,
-    "32/x2": 64,
-    "32": 32
-}
+SIZES = {"256": 256, "128": 128, "32/x4": 128, "32/x2": 64, "32": 32}
 
 
-def downscale (in_size: int, out_size: int, in_dir: path, out_dir: path):
-    if out_size > in_size: raise Exception(f"Rate should be less than or equal to {in_size}: {out_size}")
+def downscale(in_size: int, out_size: int, in_dir: path, out_dir: path):
+    if out_size > in_size:
+        raise Exception(f"Rate should be less than or equal to {in_size}: {out_size}")
     files = glob(path.join(in_dir, "*.png"))
     length = len(files)
     logger.info(f"files: {length}")
@@ -57,8 +53,9 @@ def downscale (in_size: int, out_size: int, in_dir: path, out_dir: path):
         except Exception as e:
             logger.error(f"error occurred for {fn}: {e}")
 
+
 def run(base: str, chain: str):
-    targets = [ size for size in SIZES if SIZES[size] <= SIZES[base]]
+    targets = [size for size in SIZES if SIZES[size] <= SIZES[base]]
     chains = [chain] if chain else CHAINS
     logger.info(f"targets: {targets}")
     for chain in chains:
@@ -74,15 +71,19 @@ def run(base: str, chain: str):
             logger.info(f"outer directory: {out_dir}")
             downscale(SIZES[base], SIZES[target], in_dir, out_dir)
 
+
 def main():
     parser = ArgumentParser()
     parser.add_argument("--base", type=str, default="256", required=True)
     parser.add_argument("--chain", type=str, default=None, required=False)
     args = parser.parse_args()
     logger.info(f"argument setting: {args}")
-    if args.base not in SIZES: raise Exception(f"Invalid image target: {args.base}")
-    if args.chain not in CHAINS: raise Exception(f"Invalid chain: {args.chain}")
+    if args.base not in SIZES:
+        raise Exception(f"Invalid image target: {args.base}")
+    if args.chain not in CHAINS:
+        raise Exception(f"Invalid chain: {args.chain}")
     run(args.base, args.chain)
+
 
 if __name__ == "__main__":
     main()
